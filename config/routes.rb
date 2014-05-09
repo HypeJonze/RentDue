1be6bd84ae27563e69d39b132db6358cbe7121c8
Ayhan Basmisirli <ayhan.basmisirli@gmail.com>
2014-05-09 02:05:26 -0400

    devise cleanup adding user firstname and lastname bootstrap added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
-
+  devise_for :users
+  root  'users#home' 
   resources :users do
     resources :properties
   end 
@@ -10,7 +11,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   #root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
