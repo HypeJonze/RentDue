58c5578b0c68a658b71d2d3f7c148939c04ccbc1
Ayhan Basmisirli <ayhan.basmisirli@gmail.com>
2014-05-09 21:28:42 -0400

    after sign in takes user home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  
+#after user sign in it takes to user main page
+  def after_sign_in_path_for(resource)
+      user_path(resource)
+  end
+
   protected
 
   def configure_devise_permitted_parameters
