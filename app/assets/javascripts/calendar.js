1c1fffd3a801963dd3ab6265571510229599d39b
Ayhan Basmisirli <ayhan.basmisirli@gmail.com>
2014-05-20 01:50:24 -0400

    actual adress and price show on calendar

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,7 +1,30 @@
 $(document).ready(function (){
   console.log("ayhan");
   $('#calendar').fullCalendar({
-    height: 300,
-    events: 'properties.json'
+    contentHeight: 250,
+     windowResize:true,
+
+       editable: true,
+       defaultView: 'month',
+
+
+       eventSources: [
+
+
+        {
+
+            description:'first description',
+            url: 'properties.json', 
+            
+            color: 'lightblue', 
+            textColor: 'black' 
+        }
+
+      ],
+      eventRender: function(event, element)
+    { 
+          element.find('.fc-event-inner').append("<br/>" + event.address+"<br/>"+"$"+event.rent); 
+    }
+
   });
 });
--- app/views/properties/index.json.jbuilder
+++ app/views/properties/index.json.jbuilder
@@ -1,5 +1,8 @@
 json.array!(@properties) do |property|
-  json.extract! property, :id, :utilies,:address
+  json.extract! property, :user_id,:id, :utilies,:address,:rent
   json.start property.utilies
+  json.address property.address
+  json.id property.id
+  json.user_id property.user_id
   json.url user_properties_url(property, format: :html)
 end
